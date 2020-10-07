b0bd31f282b8303025d5d7086783aad2c6e976f8
lukas.komprs <lukas.komprs@studentagency.cz>
2020-10-07 21:13:19 +0200

    Splitting an App Into Components

--- listsConditionals/src/containers/App.js
+++ listsConditionals/src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from '..components/Persons/Person/Person';
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
+import Persons from '../components/Persons/Persons';
+
 
 class App extends Component {
   state = {
@@ -53,14 +53,11 @@ class App extends Component {
       //rendering list of persons
       persons = (
         <div>
-          {this.state.persons.map((person, index) => {
-            return <Person
-              click={() => this.deletePersonHandler(index)}
-              name={person.name}
-              age={person.age}
-              key={person.id}
-              changed={(event) => this.nameChangedHandler(event, person.id)} />
-          })}
+          <Persons
+            persons = {this.state.persons}
+            clicked = {this.deletePersonHandler}
+            changed = {this.nameChangedHandler}
+          />
         </div>
       );
     }
