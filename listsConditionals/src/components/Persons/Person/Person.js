38ce5cb5f43f95401caaaaf262fbd070abdcb3e0
lukas.komprs <lukas.komprs@studentagency.cz>
2020-10-08 16:12:01 +0200

    Component Update Lifecycle for props Changes 91

--- listsConditionals/src/components/Persons/Person/Person.js
+++ listsConditionals/src/components/Persons/Person/Person.js
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, {Component} from 'react';
 import styled from 'styled-components';
 
 import './Person.css';
 
-const person = ( props ) => {
+ class Person extends Component {
+   render() {
   console.log('[Person.js] rendering ...');
-    return (
-
-          <div>
-            <p onClick={props.clicked}>I'm {props.name} and I am {props.age} years old!</p>
-            <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value={props.name} />
-        </div>
-    )
+     return (
+           <div>
+             <p onClick={this.props.clicked}>I'm {this.props.name} and I am {this.props.age} years old!</p>
+             <p>{this.props.children}</p>
+             <input type="text" onChange={this.props.changed} value={this.props.name} />
+         </div>
+     )
+   }
 };
 
-export default person;
+export default Person;
--- listsConditionals/src/components/Persons/Persons.js
+++ listsConditionals/src/components/Persons/Persons.js
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import Person from './Person/Person';
 //we do not have jsx we can ommit => ()
-const persons = (props) =>  {
-    console.log('[Persons.js] rendering ..');
- return props.persons.map((person, index) => {
-  //We got our persons which we expect to receive on our props and then we still map persons into array of JSX elements
+class Persons extends Component {
+  static getDerivedStateFromProps(props,state){
+      console.log('[Persons.js] getDerivedStateFromProps');
+      return state;
+  }
 
-    return <Person
-      clicked={() => props.clicked(index)}
+  shouldComponentUpdate(nextProps, nextState){
+    //we have to return true or false if  react should continue
+    console.log('[Persons.js] shouldComponentUpdate');
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState){
+    console.log('[Persons.js] getSnapshotBeforeUpdate');
+  }
+
+  // run when we are done with all the updating
+  componentDidMount() {
+    console.log('[Persons.js] componentDidMount');
+  }
+
+  render(){
+    console.log('[Persons.js] rendering ..');
+ return  this.props.persons.map((person, index) => {
+  //We got our persons which we expect to receive on our props and then we still map persons into array of JSX elements
+    return (<Person
+      clicked={() => this.props.clicked(index)}
       name={person.name}
       age={person.age}
       key={person.id}
-      changed={(event) => props.changed(event, person.id)} />
-  });
+      changed={(event) => this.props.changed(event, person.id)} />
+      )
+    });
+  }
 };
-  export default persons;
+  export default Persons;
