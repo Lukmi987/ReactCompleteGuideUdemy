825d0f0e7000308cbdc4a290107b0e235cb0016f
lukas.komprs <lukas.komprs@studentagency.cz>
2020-10-09 13:03:41 +0200

    UseEffect() in Functionl Components

--- listsConditionals/src/components/Cockpit/Cockpit.js
+++ listsConditionals/src/components/Cockpit/Cockpit.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 //turn array of strings into one string, generic solution to manipulate dinamically
 const classes = [];
 
-
+//useEffect is the second most important React hook you can use next to useState because useEffect and now that sounds strange but useEffect basically combines the functionality or the use cases you can cover
+// of all these class-based lifecycle hooks in one React hook here and both is called hook,
 
 const cockpit = (props) => {
 
