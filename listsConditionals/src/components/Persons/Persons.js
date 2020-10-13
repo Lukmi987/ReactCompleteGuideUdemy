4185325732b73889c4a398d1a6816afe0809cad7
lukas.komprs <lukas.komprs@studentagency.cz>
2020-10-13 20:22:44 +0200

    PureComponent last commit not saved good

--- listsConditionals/src/components/Persons/Persons.js
+++ listsConditionals/src/components/Persons/Persons.js
@@ -1,8 +1,8 @@
-import React, {Component} from 'react';
-
+import React, {PureComponent} from 'react';
+// PureComponent just a normal component that already implements shouldComponentUpdate with a complete props check
 import Person from './Person/Person';
 //we do not have jsx we can ommit => ()
-class Persons extends Component {
+class Persons extends PureComponent {
   // static getDerivedStateFromProps(props,state){
   //     console.log('[Persons.js] getDerivedStateFromProps');
   //     return state;
@@ -10,17 +10,17 @@ class Persons extends Component {
 
 
 /// !! When we update in input field updating process start here coz we pass here the updated state a lifecycle begin
-  shouldComponentUpdate(nextProps, nextState){
-    //we have to return true or false if  react should continue
-    console.log('[Persons.js] shouldComponentUpdate');
-    //what it compares are pointers actually , that's why we should defined changes dynamically creating copies, coz objects and arrays are referenced type in JS
-    if (nextProps.persons !== this.props.persons){
-      return true;
-    }else{
-        return false;
-      }
-
-  }
+  // shouldComponentUpdate(nextProps, nextState){
+  //   //we have to return true or false if  react should continue
+  //   console.log('[Persons.js] shouldComponentUpdate');
+  //   //what it compares are pointers actually , that's why we should defined changes dynamically creating copies, coz objects and arrays are referenced type in JS
+  //   if (nextProps.persons !== this.props.persons){
+  //     return true;
+  //   }else{
+  //       return false;
+  //     }
+  //
+  // }
 
   getSnapshotBeforeUpdate(prevProps, prevState){
     // snapshot is a data package which we then recieve in componentDidUpdate, to save some state before the update such as scroll position
