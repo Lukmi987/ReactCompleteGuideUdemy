7a1fbc07b466a9bbcda3a70cf28e687e208ffd8b
lukas.komprs <lukas.komprs@studentagency.cz>
2020-10-13 22:19:01 +0200

    Aux

--- listsConditionals/src/components/Persons/Person/Person.js
+++ listsConditionals/src/components/Persons/Person/Person.js
@@ -1,18 +1,19 @@
 import React, {Component} from 'react';
 import styled from 'styled-components';
-import Aux from '../../hoc/Aux';
+import Aux from '../../../hoc/Aux';
 
 import './Person.css';
 
  class Person extends Component {
    render() {
   console.log('[Person.js] rendering ...');
-     return
-          [
-             <p key="i1" onClick={this.props.clicked}>I'm {this.props.name} and I am {this.props.age} years old!</p>,
-             <p> {this.props.children}</p>,
+     return (
+          <Aux>
+             <p key="i1" onClick={this.props.clicked}>I'm {this.props.name} and I am {this.props.age} years old!</p>
+             <p> {this.props.children}</p>
              <input key="i3" type="text" onChange={this.props.changed} value={this.props.name} />
-         ];
+          </Aux>
+         );
    }
 }
 
--- listsConditionals/src/hoc/Aux.js
+++ listsConditionals/src/hoc/Aux.js
@@ -1,4 +1,4 @@
-import React From 'react';
+import React from 'react';
 //Aux an empty wrapper using a special childre property, which react reserve for us
 // and children will always refer between opening and closing tag of your component
 const aux = props => props.children;
