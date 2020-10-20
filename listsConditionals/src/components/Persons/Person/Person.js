653303302483565280154ee090535b191a29ba60
lukas.komprs <lukas.komprs@studentagency.cz>
2020-10-20 20:32:51 +0200

    Using Refs

--- listsConditionals/src/components/Persons/Person/Person.js
+++ listsConditionals/src/components/Persons/Person/Person.js
@@ -7,13 +7,27 @@ import PropTypes from 'prop-types'
 import './Person.css';
 
  class Person extends Component {
+
+   constructor(props){
+     super(props);
+     this.inputElementRef = React.createRef();
+   }
+
+   componentDidMount(){
+  //   this.inputElement.focus();
+  this.inputElementRef.current.focus();
+   }
+
    render() {
   console.log('[Person.js] rendering ...');
      return (
           <Aux>
              <p key="i1" onClick={this.props.clicked}>I'm {this.props.name} and I am {this.props.age} years old!</p>
              <p> {this.props.children}</p>
-             <input key="i3" type="text" onChange={this.props.changed} value={this.props.name} />
+             <input key="i3"
+               ref={this.inputElementRef}
+                type="text"
+                onChange={this.props.changed} value={this.props.name} />
           </Aux>
          );
    }
