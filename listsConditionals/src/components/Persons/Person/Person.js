83e85e91568acb39a314261074becc79d690d5db
lukas.komprs <lukas.komprs@studentagency.cz>
2020-10-20 20:17:45 +0200

    Using propTypes to get warning if we pass a wrong type

--- listsConditionals/src/components/Persons/Person/Person.js
+++ listsConditionals/src/components/Persons/Person/Person.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
 import styled from 'styled-components';
-import withClass from '../../../hoc/withClass'
+import withClass from '../../../hoc/withClass';
 import Aux from '../../../hoc/Aux';
+import PropTypes from 'prop-types'
 
 import './Person.css';
 
@@ -18,4 +19,11 @@ import './Person.css';
    }
 }
 
+Person.propTypes = {
+  //on a click I expect to get a pointer to a func
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  changed: PropTypes.func
+};
 export default Person;
