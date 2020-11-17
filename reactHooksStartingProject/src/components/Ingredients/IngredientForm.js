d4311622b20d1f710b3bfeebcb423101e1a09c51
lukas.komprs <lukas.komprs@studentagency.cz>
2020-11-17 20:12:35 +0100

    Updating state , properly solving closure problems

--- reactHooksStartingProject/src/components/Ingredients/IngredientForm.js
+++ reactHooksStartingProject/src/components/Ingredients/IngredientForm.js
@@ -18,11 +18,31 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" value={inputState[0].title} onChange={event => inputState[1]({title: event.target.value, amount: inputState[0].amount})} />
+            <input type="text" id="title" value={inputState[0].title}
+              onChange={event => {
+                const newTitle=  event.target.value;
+                inputState[1]( prevInputState => ({
+                title: newTitle,
+                amount: prevInputState.amount
+              }))
+            }}
+               />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" value={inputState[0].amount} onChange={event => inputState[1]({amount:event.target.value,title: inputState[0].title})} />
+            <input type="number" id="amount" value={inputState[0].amount}
+              onChange={event => {
+                const newAmount = event.target.value;
+                //we are calling set state function and there we define  another nested function(updating func)
+                //problem when we call from inner func something from outside func such as : event,
+                //!! instead of creating a new event object for every keystroke it instead reuses the previous object and the consequence of this is that for the second keystroke
+                // we still reuse that same object we had for first keystroke, ,we are reusing wrong event object
+                inputState[1]( prevInputState => ({
+                  amount:newAmount,
+                  title: prevInputState.title
+                }))}
+              }
+              />
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
