c9bde801efba01d6a67b5e72355c2432542844b3
lukas.komprs <lukas.komprs@studentagency.cz>
2020-11-19 09:30:23 +0100

    Passing state data accross components

--- reactHooksStartingProject/src/components/Ingredients/Ingredients.js
+++ reactHooksStartingProject/src/components/Ingredients/Ingredients.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
 import Search from './Search';
 
-function Ingredients() {
+const Ingredients = () => {
+  const [userIngredients, setUserIngredients] = useState([]);
+
+  const addIngredientHandler = ingredient => {
+    //taking all elements of the old array and make a new array with new element
+    setUserIngredients(prevIngredients => [
+       ...prevIngredients,
+       // ...ingredient takes all key /value pairs of the object and adds them as key/value as a new object
+       {id: Math.random().toString(), ...ingredient}]);
+  }
+  console.log(userIngredients);
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search />
+        <IngredientList ingredients={userIngredients} onRemoveItem={() => {}} />
         {/* Need to add list here! */}
       </section>
     </div>
