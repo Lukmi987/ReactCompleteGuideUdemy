3cfb2c66d3db38b8e5e1702ccf6dd0db545c7d5a
lukas.komprs <lukas.komprs@studentagency.cz>
2020-11-19 08:20:19 +0100

    multiple States

--- reactHooksStartingProject/src/components/Ingredients/IngredientForm.js
+++ reactHooksStartingProject/src/components/Ingredients/IngredientForm.js
@@ -6,7 +6,8 @@ import './IngredientForm.css';
 const IngredientForm = React.memo(props => {
   //alway return an array with 2 elements, first the snapshot of current state and second we use to set a state
   //array destructuring
-  const [inputState, setInputState] = useState({title: '',amount:''});
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
 
   const submitHandler = event => {
     event.preventDefault();
@@ -19,29 +20,18 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" value={inputState.title}
+            <input type="text" id="title" value={enteredTitle}
               onChange={event => {
-                const newTitle=  event.target.value;
-                setInputState( prevInputState => ({
-                title: newTitle,
-                amount: prevInputState.amount
-              }))
+              setEnteredTitle(event.target.value);
             }}
                />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" value={inputState.amount}
+            <input type="number" id="amount" value={enteredAmount}
               onChange={event => {
-                const newAmount = event.target.value;
-                //we are calling set state function and there we define  another nested function(updating func)
-                //problem when we call from inner func something from outside func such as : event,
-                //!! instead of creating a new event object for every keystroke it instead reuses the previous object and the consequence of this is that for the second keystroke
-                // we still reuse that same object we had for first keystroke, ,we are reusing wrong event object
-                setInputState( prevInputState => ({
-                  amount:newAmount,
-                  title: prevInputState.title
-                }))}
+                setEnteredAmount(event.target.value);
+                }
               }
               />
           </div>
