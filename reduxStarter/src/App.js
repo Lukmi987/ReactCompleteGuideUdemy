564002b8bdad76ffb322a7ad612b0241ef636fe1
Lukas <>
2020-12-06 20:38:04 +0100

    Adding a logOut link and Updating the UI depending on Auth state

--- reduxStarter/src/App.js
+++ reduxStarter/src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useDispatch, useSelector } from "react-redux";
 import Tabs from 'react-bootstrap/Tabs';
 import Tab from 'react-bootstrap/Tab';
 import Counter from './app/counter/containers//Counter';
@@ -6,28 +7,53 @@ import UserForm from "./app/users/containers/UserForm";
 import UserList from './app/users/containers/UserList';
 import AuthForm from "./app/auth/containers/AuthForm";
 import Locations from "./app/ybusLocations/containers/Locations";
+import store from './configureStore';
+import {logOut} from "./app/auth/actions";
 import './App.css';
 
-const App = () =>  (
-    <div className="App">
-        <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
-            <Tab eventKey="home" title="Home">
-                <Counter />
-            </Tab>
-            <Tab eventKey="profile" title="User Form">
-                <UserForm />
-            </Tab>
-            <Tab eventKey="contact" title="User List" >
-                <UserList />
-            </Tab>
-            <Tab eventKey="signUp" title="Sign Up" >
-            <AuthForm />
-            </Tab>
-            <Tab eventKey="YbusLocations" title="Ybus Locations" >
-                <Locations />
-            </Tab>
-        </Tabs>
-    </div>
-)
 
+const App = () =>  {
+  const state = store.getState();
+  const dispatch = useDispatch();
+  const logUserOut = () => dispatch(logOut());
+  const test = () => console.log('click in app');
+  //const idToken = state.authentication.userAuthInfo.idToken;
+  //better to use useSelector or store.getState() , why it is not rerendered using store.getState();?
+
+const idToken = useSelector(state => state.authentication.userAuthInfo.idToken);
+  useEffect(()=>{
+    console.log('hello from app in use effect',idToken);
+
+
+  },[idToken,logUserOut])
+
+  //how to get state  here ?
+    return(
+        <div className="App">
+            <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
+                <Tab eventKey="home" title="Home">
+                    <Counter />
+                </Tab>
+                <Tab eventKey="profile" title="User Form">
+                    <UserForm />
+                </Tab>
+                <Tab eventKey="contact" title="User List" >
+                    <UserList />
+                </Tab>
+                {!idToken ?
+                <Tab eventKey="authenticate" title="Authenticate" >
+                <AuthForm />
+                </Tab>
+                :
+                <Tab  eventKey="authenticate" title="Log Out" >
+                  <button onClick={logUserOut}>LogOut</button>
+                </Tab>
+              }
+                <Tab eventKey="YbusLocations" title="Ybus Locations" >
+                    <Locations />
+                </Tab>
+            </Tabs>
+        </div>
+  )
+}
 export default App;
